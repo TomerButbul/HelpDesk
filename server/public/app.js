aa611804e6be71983664bec20a01e1522ee52bfb
Tomer <151302269+TomerButbul@users.noreply.github.com>
2024-04-30 17:30:44 -0400

    Update app.js

--- server/public/app.js
+++ server/public/app.js
@@ -8,7 +8,6 @@ const activity = document.querySelector('.activity')
 const usersList = document.querySelector('.user-list')
 const roomList = document.querySelector('.room-list')
 const chatDisplay = document.querySelector('.chat-display')
-
 function sendMessage(e) {
     e.preventDefault()
     if (nameInput.value && msgInput.value && chatRoom.value) {
